38159adf640e31f29cfae93181c6ded96d7239bb
Anett Gesierich <anett@codemonauts.com>
2018-10-27 18:25:44 +0200

    Init w/ welcome message

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <MonsterMelodies msg="Welcome to MonsterMelodies"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MonsterMelodies from './components/MonsterMelodies.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MonsterMelodies
   }
 }
 </script>
---
+++ src/components/MonsterMelodies.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MonsterMelodies',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
