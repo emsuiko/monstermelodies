11d24443c537286aebfad85eb4e2251e76170823
Anett Gesierich <miko@emsuiko.de>
2018-11-06 00:29:11 +0100

    style btn

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <navigation/>
-    <router-view/>
-    <mome-footer/>
-  </div>
+    <div id="app">
+        <navigation/>
+        <router-view/>
+        <mome-footer/>
+    </div>
 </template>
 
 <script>
 import Navigation from "./components/Navigation.vue";
 import MomeFooter from "./components/Footer.vue";
 export default {
-  name: 'app',
-  components: {
-    Navigation,
-    MomeFooter
-  }
+    name: 'app',
+    components: {
+        Navigation,
+        MomeFooter
+    }
 }
 </script>
 
 <style lang="sass">
 body
-  margin: 0
-  background-color: #f8f5de
-  background-image: linear-gradient(to right, rgba(255,210,0,0.4), rgba(200, 160, 0, 0.1) 11%, rgba(0,0,0,0) 35%, rgba(200, 160, 0, 0.1) 65%)
-  box-shadow: inset 0 0 75px rgba(255,210,0,0.3), inset 0 0 20px rgba(255,210,0,0.4), inset 0 0 30px rgba(220,120,0,0.8)
-  min-height: 100vh
+    margin: 0
+    background-color: #f8f5de
+    background-image: linear-gradient(to right, rgba(255,210,0,0.4), rgba(200, 160, 0, 0.1) 11%, rgba(0,0,0,0) 35%, rgba(200, 160, 0, 0.1) 65%)
+    box-shadow: inset 0 0 75px rgba(255,210,0,0.3), inset 0 0 20px rgba(255,210,0,0.4), inset 0 0 30px rgba(220,120,0,0.8)
+    min-height: 100vh
 #app
-    font-family: 'Uncial Antiqua', cursive;
+    font-family: 'Uncial Antiqua', cursive
     text-align: center
     color: #2c3e50
     display: grid
@@ -37,12 +37,20 @@ body
 a
     color: #2c3e50
     text-decoration: none
+
+input, button, a
+    font-family: 'Uncial Antiqua', cursive
+
 .btn
-    border: 2px solid black
+    border: 2px solid #2F4858
     text-decoration: none
-    padding: .5rem
-    background-color: white
+    padding: .75rem
+    margin: .75rem
+    background-color: #C5D2BD
     display: inline-block
+    font-size: 1.5rem
+    border-radius: 1rem
+
 .hide
     display: none
 </style>
