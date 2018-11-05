d8e8bcde68e85591ce7b02b49a7397c365eb8f63
Anett Gesierich <miko@emsuiko.de>
2018-11-06 00:22:40 +0100

    extend frontpage

--- src/views/MonsterMelodies.vue
+++ src/views/MonsterMelodies.vue
@@ -1,19 +1,26 @@
 <template lang="pug">
-  div.general
-    h1 Monster&thinsp;Melodies
-    img(alt="melodia" src="./../assets/melodia.png")
-    div
-      router-link(to="/game" class="btn") Start
+    .general
+        h1 Willkommen bei Monster&thinsp;Melodies!
+        p
+            | Nunc sollicitudin luctus odio at euismod. Aliquam erat volutpat. Aliquam rhoncus egestas orci ac ultricies. Quisque mauris odio, cursus id varius ut, euismod vel dolor.
+        img(alt="melodia" src="./../assets/melodia.png")
+        div
+            router-link(to="/game" class="btn") Start
 </template>
 
 <script>
 export default {
-  name: 'MonsterMelodies',
+    name: 'MonsterMelodies',
 }
 </script>
 
 <style scoped lang="sass">
-img
-  max-width: 80%;
-  height: auto;
+.general
+    display: grid
+    grid-template-rows: repeat(4, auto)
+    grid-template-colums: 1fr
+    h1
+        align-self: center
+    img
+        justify-self: center
 </style>
